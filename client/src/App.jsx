dccb013e360e39de59388d66cbcfdab701a6bd45
nishant-Tiwari24 <nishant_tiwari@srmap.edu.in>
2024-09-15 13:15:46 +0530

    feat: meta data updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
 import Navbar from "./components/layout/Navbar";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Upload from "./components/upload/Upload";
 import AllFileData from "./components/fileData/AllFileData";
 import PartitionTable from "./components/partition/PartitionTable";
 import SampleInfo from "./components/sample/ImageInfo";
+import MetadataTable from "./components/metadata/MetaData";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-         <Route path='/' element={<Home/>} />
-         <Route path='/upload' element={<Upload/>} />
-         <Route path='/disk-file-data' element={<AllFileData/>} />
-         <Route path='/partition-table' element={<PartitionTable/>} />
-         <Route path='/sample-info' element={<SampleInfo/>} />
-       </Routes>
-       <Footer />
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/disk-file-data" element={<AllFileData />} />
+        <Route path="/partition-table" element={<PartitionTable />} />
+        <Route path="/sample-info" element={<SampleInfo />} />
+        <Route path="/meta-data" element={<MetadataTable />} />
+      </Routes>
+      <Footer />
     </>
   );
 };
